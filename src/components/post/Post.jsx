eb597c7391fef81da8ca19c416b0c4b7a2c669ca
israelsanttana <israelsantanasantos7@gmail.com>
2023-04-12 17:10:45 -0300

    feat: adiciona pubilação dinamica

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,12 +1,17 @@
 import { Avatar } from '../avatar/Avatar'
 import { Comment } from '../comment/Comment'
 import styles from './Post.module.css'
-import { format } from 'date-fns'
+import { format, formatDistanceToNow } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
 
-export function Post({ author, publishedAt }) {
+export function Post({ author, publishedAt, content }) {
 
-    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", { locale: ptBR })
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", { locale: ptBR });
+
+    const plublishDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true,
+    })
 
 
     return (
@@ -20,18 +25,21 @@ export function Post({ author, publishedAt }) {
                     </div>
 
                 </div>
-                <time title='2023-04=04 19:38:30' dateTime='2023-04=04 19:38:30'>{publishedDateFormatted}
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {plublishDateRelativeToNow}
                 </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p> <a href="#">jane.design/doctorcare</a></p>
-                <p> <a href="#">#novoprojeto</a> {' '}
-                    <a href="#">#nlw</a> {' '}
-                    <a href="#">#rocketseat</a> {' '}
-                </p>
+
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } else if (line.type === 'link') {
+                        return <p><a href="#">{line.content}</a></p>
+                    }
+                })}
+
 
             </div>
 
