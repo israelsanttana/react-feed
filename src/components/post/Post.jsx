27168ca6db3856becffb0997ddc4626c799ce457
israelsanttana <israelsantanasantos7@gmail.com>
2023-04-27 19:59:55 -0300

    feat: adiciona key ao comments list

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -76,7 +76,7 @@ export function Post({ author, publishedAt, content }) {
 
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment} />
+                    return <Comment key={comment} content={comment} />
                 })}
 
             </div>
