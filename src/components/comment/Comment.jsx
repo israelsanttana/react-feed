97b3bbaf569ccae18b39d17069871401275fcad2
israelsanttana <israelsantanasantos7@gmail.com>
2023-05-04 23:36:33 -0300

    fix: reformula função de likes

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -12,7 +12,9 @@ export function Comment({ content, deleteComment }) {
     };
 
     function handleLikeCommet() {
-        setLikeCount(likeCount + 1);
+        setLikeCount((state) => {
+            return state + 1
+        });
     }
 
 
